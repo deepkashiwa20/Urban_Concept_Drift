f54de5308586d870a0512611c7cdcffa830b2dd0
Renhe Jiang <54202779+deepkashiwa20@users.noreply.github.com>
2023-10-18 01:43:37 +0900

    Add files via upload

---
+++ code/setting/Param.py
@@ -0,0 +1,22 @@
+# General
+TIMESTEP_IN = 12
+TIMESTEP_OUT = 12
+BATCHSIZE = 64
+LEARN = 0.001
+EPOCH = 200
+PATIENCE = 10
+OPTIMIZER = 'Adam'
+LOSS = 'MAE'
+TRAINVALSPLIT = 0.2
+
+
+# MemDA
+DATA_SET = 'Beijing'
+GPU = '3'
+encoder = 'gwn'
+encoder_dim = 256
+look_back = 2
+mem_num = 20
+mem_dim = 32
+ntn_dim = 32
+ntn_k = 5
